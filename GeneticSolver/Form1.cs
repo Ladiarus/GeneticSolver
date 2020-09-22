c49856251f5790fb027689667116b0c7864ba69c
Ladiarus <yarlad03@gmail.com>
2020-09-22 22:31:08 +0300

    ExtractGamets

--- GeneticSolver/Form1.cs
+++ GeneticSolver/Form1.cs
@@ -12,9 +12,60 @@ namespace GeneticSolver
 {
     public partial class Form1 : Form
     {
+        string gen1, gen2;
+        string gamets1 = "", gamets2 = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            Generate();
+        }
+        private void Generate()
+        {
+            listBox1.Items.Clear();
+            listView1.Items.Clear();
+            gen1 = Gen1.Text;
+            gen2 = Gen2.Text;
+            ExtractGamets();
+            for (int i = 0; i < (gamets1 != "" ? 1:0); i++)
+            {
+                listView1.Items.Add(gamets1);
+                listView1.Items.Add(gamets2);
+                listBox1.Items.Add(gamets1);
+                listBox1.Items.Add(gamets2);
+            }
+        }
+        void ExtractGamets()
+        {
+            gamets1 = "";
+            gamets2 = "";
+            try
+            {
+                gamets1 += gen1[0];
+                gamets2 += gen2[0];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("WTF?\nEnter Gens");
+                return;
+            }
+            for (int i = 1; i < gen1.Length; i++)
+            {
+                if (gamets1[gamets1.Length - 1] != gen1[i])
+                {
+                    gamets1 += gen1[i];
+                }
+            }
+            for (int i = 1; i < gen2.Length; i++)
+            {
+                if (gamets2[gamets2.Length - 1] != gen2[i])
+                {
+                    gamets2 += gen2[i];
+                }
+            }
         }
     }
 }
