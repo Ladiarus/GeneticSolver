59e44389e990684078fd701beac7f8cf769b24d0
Ladiarus <43500395+Ladiarus@users.noreply.github.com>
2020-09-23 10:53:41 +0300

    fixed

--- GeneticSolver/Form1.Designer.cs
+++ GeneticSolver/Form1.Designer.cs
@@ -28,45 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.generateButton = new System.Windows.Forms.Button();
             this.Gen1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.Gen2 = new System.Windows.Forms.TextBox();
-            this.GenerateButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // generateButton
+            // 
+            this.generateButton.BackColor = System.Drawing.SystemColors.Control;
+            this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.generateButton.Location = new System.Drawing.Point(285, 173);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(172, 77);
+            this.generateButton.TabIndex = 0;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = false;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
             // 
             // Gen1
             // 
-            this.Gen1.Location = new System.Drawing.Point(12, 22);
+            this.Gen1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Gen1.Location = new System.Drawing.Point(51, 88);
             this.Gen1.Name = "Gen1";
-            this.Gen1.Size = new System.Drawing.Size(244, 20);
-            this.Gen1.TabIndex = 0;
+            this.Gen1.Size = new System.Drawing.Size(185, 29);
+            this.Gen1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(96, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Genotype 1 ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(543, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Genotype 2";
             // 
             // Gen2
             // 
-            this.Gen2.Location = new System.Drawing.Point(544, 22);
+            this.Gen2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Gen2.Location = new System.Drawing.Point(491, 88);
             this.Gen2.Name = "Gen2";
-            this.Gen2.Size = new System.Drawing.Size(244, 20);
-            this.Gen2.TabIndex = 1;
+            this.Gen2.Size = new System.Drawing.Size(185, 29);
+            this.Gen2.TabIndex = 4;
             // 
-            // GenerateButton
+            // listView1
             // 
-            this.GenerateButton.Location = new System.Drawing.Point(291, 22);
-            this.GenerateButton.Name = "GenerateButton";
-            this.GenerateButton.Size = new System.Drawing.Size(220, 72);
-            this.GenerateButton.TabIndex = 2;
-            this.GenerateButton.Text = "Genrate";
-            this.GenerateButton.UseVisualStyleBackColor = true;
-            this.GenerateButton.Click += new System.EventHandler(this.button1_Click);
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(524, 134);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(250, 304);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.ColumnWidth = 100;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 134);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(139, 303);
+            this.listBox1.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.Gen2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Gen1);
+            this.Controls.Add(this.generateButton);
             this.Name = "Form1";
-            this.Text = "GeneticSolver";
+            this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,9 +125,13 @@
 
         #endregion
 
+        private System.Windows.Forms.Button generateButton;
         private System.Windows.Forms.TextBox Gen1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox Gen2;
-        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
