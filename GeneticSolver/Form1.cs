a3f95c6125a3cce72871a9ea997c831b629d04a1
Ladiarus <43500395+Ladiarus@users.noreply.github.com>
2020-09-23 10:18:24 +0300

    test

--- GeneticSolver/Form1.cs
+++ GeneticSolver/Form1.cs
@@ -12,11 +12,15 @@ namespace GeneticSolver
 {
     public partial class Form1 : Form
     {
+<<<<<<< HEAD
         string g1, g2;
+=======
+>>>>>>> parent of c498562... ExtractGamets
         public Form1()
         {
             InitializeComponent();
         }
+<<<<<<< HEAD
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -24,5 +28,7 @@ namespace GeneticSolver
             g2 = Gen2.Text;
             MessageBox.Show(Gen1.Text + " " + Gen2.Text);
         }
+=======
+>>>>>>> parent of c498562... ExtractGamets
     }
 }
