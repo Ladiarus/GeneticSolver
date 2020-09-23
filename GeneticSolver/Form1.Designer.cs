7a9a4cc47d53e4b98d4ec3f7c659dc5cc61823db
Ladiarus <43500395+Ladiarus@users.noreply.github.com>
2020-09-23 10:45:53 +0300

    Revert "test"
    This reverts commit a3f95c6125a3cce72871a9ea997c831b629d04a1.

--- GeneticSolver/Form1.Designer.cs
+++ GeneticSolver/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
             this.Gen1 = new System.Windows.Forms.TextBox();
             this.Gen2 = new System.Windows.Forms.TextBox();
             this.GenerateButton = new System.Windows.Forms.Button();
@@ -58,35 +57,26 @@
             this.GenerateButton.UseVisualStyleBackColor = true;
             this.GenerateButton.Click += new System.EventHandler(this.button1_Click);
             // 
-=======
-            this.SuspendLayout();
-            // 
->>>>>>> parent of c498562... ExtractGamets
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-<<<<<<< HEAD
             this.Controls.Add(this.GenerateButton);
             this.Controls.Add(this.Gen2);
             this.Controls.Add(this.Gen1);
-=======
->>>>>>> parent of c498562... ExtractGamets
             this.Name = "Form1";
             this.Text = "GeneticSolver";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-<<<<<<< HEAD
 
         private System.Windows.Forms.TextBox Gen1;
         private System.Windows.Forms.TextBox Gen2;
         private System.Windows.Forms.Button GenerateButton;
-=======
->>>>>>> parent of c498562... ExtractGamets
     }
 }
 
--- GeneticSolver/Form1.cs
+++ GeneticSolver/Form1.cs
@@ -12,15 +12,11 @@ namespace GeneticSolver
 {
     public partial class Form1 : Form
     {
-<<<<<<< HEAD
         string g1, g2;
-=======
->>>>>>> parent of c498562... ExtractGamets
         public Form1()
         {
             InitializeComponent();
         }
-<<<<<<< HEAD
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -28,7 +24,5 @@ namespace GeneticSolver
             g2 = Gen2.Text;
             MessageBox.Show(Gen1.Text + " " + Gen2.Text);
         }
-=======
->>>>>>> parent of c498562... ExtractGamets
     }
 }
